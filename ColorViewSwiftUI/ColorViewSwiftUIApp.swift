5fee8b59392177e572a6c91bb86ff078869d81c9
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-12 19:17:28 +0300

    Initial Commit

---
+++ ColorViewSwiftUI/ColorViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewSwiftUIApp.swift
+//  ColorViewSwiftUI
+//
+//  Created by Ilnur on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
